150b9fa2b631b8063a899782327789b6fa7f3656
Tran The Trong <trongtt@gmail.com>
2011-03-02 19:40:07 +0700

    use Scope.CHILDREN for navi portlet and fix some bugs in navi portlet

--- webui/portal/src/main/java/org/exoplatform/portal/webui/navigation/UIPortalNavigation.java
+++ webui/portal/src/main/java/org/exoplatform/portal/webui/navigation/UIPortalNavigation.java
@@ -131,7 +131,7 @@ public class UIPortalNavigation extends UIComponent
                continue;
             }
             
-            Node rootNode = naviSer.load(navi, Scope.ALL);
+            Node rootNode = naviSer.load(navi, Scope.CHILDREN);
             PageNavigationUtils.filter(rootNode, null);
             nodes.add(rootNode);
          }
