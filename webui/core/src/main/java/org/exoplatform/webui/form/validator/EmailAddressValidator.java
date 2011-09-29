31ea4c9628ceb4fab42c9332aea2214d4091e311
Hai Thanh Nguyen <haint@exoplatform.com>
2011-09-29 13:51:28 +0700

    EXOGTN-714 Email address validator allow double dash maybe occurs sql injection

--- webui/core/src/main/java/org/exoplatform/webui/form/validator/EmailAddressValidator.java
+++ webui/core/src/main/java/org/exoplatform/webui/form/validator/EmailAddressValidator.java
@@ -25,6 +25,7 @@ import org.exoplatform.webui.core.UIComponent;
 import org.exoplatform.webui.exception.MessageException;
 import org.exoplatform.webui.form.UIForm;
 import org.exoplatform.webui.form.UIFormInput;
+import org.jmock.core.constraint.IsLessThan;
 
 /**
  * Created by The eXo Platform SARL
@@ -33,12 +34,20 @@ import org.exoplatform.webui.form.UIFormInput;
  * Jun 7, 2006
  * 
  * Validates whether an email is in the correct format
+ * Valid characters that can be used in a domain name are:
+ *     a-z
+ *     0-9
+ *     - (dash) or . (dot) but not as a starting or ending character
+ *     . (dot) as a separator for the textual portions of a domain name
+ *     
+ * Valid characters that can be used in a domain name are:
+ *     a-z
+ *     0-9
+ *     _ (underscore) or .  (dot) but not as a starting or ending character
  */
 @Serialized
 public class EmailAddressValidator implements Validator
 {
-
-   static private final String EMAIL_REGEX = "[_A-Za-z0-9-]+(\\.[_A-Za-z0-9-]+)*@[_A-Za-z0-9-.]+";
 
    public void validate(UIFormInput uiInput) throws Exception
    {
@@ -54,13 +63,90 @@ public class EmailAddressValidator implements Validator
       {
          label = uiInput.getName();
       }
+      Object[] args = {label};
+      
       if (uiInput.getValue() == null || ((String)uiInput.getValue()).trim().length() == 0)
          return;
+      
       String s = (String)uiInput.getValue();
-      if (s.matches(EMAIL_REGEX))
+      int atIndex = s.indexOf('@'); 
+      if(atIndex == -1)
+         throw new MessageException(new ApplicationMessage("EmailAddressValidator.msg.Invalid-input", args,
+            ApplicationMessage.WARNING));
+      
+      String emailName = s.substring(0, atIndex);
+      String emailAddress = s.substring(atIndex + 1);
+      
+      if(emailAddress.lastIndexOf('.') == -1)
+         throw new MessageException(new ApplicationMessage("EmailAddressValidator.msg.Invalid-input", args,
+            ApplicationMessage.WARNING));
+      
+      if (checkEmail(emailName.toCharArray(), emailAddress.toCharArray()))
          return;
-      Object[] args = {label};
       throw new MessageException(new ApplicationMessage("EmailAddressValidator.msg.Invalid-input", args,
          ApplicationMessage.WARNING));
+   }
+   
+   private boolean checkEmail(char[] emailName, char[] emailAddress)
+   {
+      if(!isAlphabet(emailName[0]) || !isAlphabet(emailAddress[0])) return false;
+      else if(!isAlphabet(emailName[emailName.length - 1]) && !isDigit(emailName[emailName.length - 1])) return false;
+      else if(!isAlphabet(emailAddress[emailAddress.length - 1]) && !isDigit(emailAddress[emailAddress.length - 1])) return false;
+      else 
+      {
+         for(int i = 0; i < emailName.length; i++)
+         {
+            if(isDigit(emailName[i]) || isAlphabet(emailName[i])) continue;
+            else if(isEmailNameSymbol(emailName[i]) && !isEmailNameSymbol(emailName[i + 1])) continue;
+            return false;
+         }
+         
+         for(int i = 0; i < emailAddress.length; i++)
+         {
+            if(isDigit(emailAddress[i]) || isAlphabet(emailAddress[i])) continue;
+            else if(isEmailAddressSymbol(emailAddress[i]) && !isEmailAddressSymbol(emailAddress[i + 1])) continue;
+            return false;
+         }
+         
+         int lastDot = -1;
+         for(int i = emailAddress.length - 1; i >=0; i--)
+         {
+            if(emailAddress[i] == '.') lastDot = i;
+         }
+         
+         if(lastDot == -1) return false;
+         else
+         {
+            for(int i= emailAddress.length - 1; i >= lastDot; i--)
+            {
+               if(emailAddress[i] == '-') return false;
+            }
+         }
+      }
+      return true;
+   }
+   
+   private boolean isAlphabet(char c)
+   {
+      if(c >= 'a' && c <='z') return true;
+      return false;
+   }
+   
+   private boolean isDigit(char c)
+   {
+      if(c >= '0' && c <= '9') return true;
+      return false;
+   }
+   
+   private boolean isEmailNameSymbol(char c)
+   {
+      if(c == '_' || c == '.') return true;
+      return false;
+   }
+   
+   private boolean isEmailAddressSymbol(char c)
+   {
+      if(c == '-' || c == '.') return true;
+      return false;
    }
 }
