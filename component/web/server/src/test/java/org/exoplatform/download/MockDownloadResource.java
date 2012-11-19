729ceb993833af1780f1cc2b6cfecf9115337927
Minh Hoang TO <hoang281283@gmail.com>
2012-11-19 16:09:03 +0700

    EXOGTN-1276: Memory leak as too many InputStreamDownloadResource resources are added into DownloadService

--- component/web/server/src/test/java/org/exoplatform/download/MockDownloadResource.java
+++ component/web/server/src/test/java/org/exoplatform/download/MockDownloadResource.java
@@ -1,51 +1,49 @@
-/**
- * Copyright (C) 2009 eXo Platform SAS.
- * 
+/*
+ * Copyright (C) 2012 eXo Platform SAS.
+ *
  * This is free software; you can redistribute it and/or modify it
  * under the terms of the GNU Lesser General Public License as
  * published by the Free Software Foundation; either version 2.1 of
  * the License, or (at your option) any later version.
- * 
+ *
  * This software is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
  * Lesser General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU Lesser General Public
  * License along with this software; if not, write to the Free
  * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
  * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
  */
-
 package org.exoplatform.download;
 
+import java.io.IOException;
 import java.io.InputStream;
 
 /**
- * Created by The eXo Platform SARL
- * Author : Tuan Nguyen
- *          tuan08@users.sourceforge.net
- * Dec 26, 2005
- *
- * @deprecated It is better to avoid using this download resource
+ * @author <a href="hoang281283@gmail.com">Minh Hoang TO</a>
+ * @date 11/19/12
  */
-public class InputStreamDownloadResource extends DownloadResource
+public class MockDownloadResource extends DownloadResource
 {
-   private InputStream is_;
+   private String id;
 
-   public InputStreamDownloadResource(InputStream is, String resourceMimeType)
+   public MockDownloadResource(String id)
    {
-      this(null, is, resourceMimeType);
+      super(null);
+      this.id = id;
    }
 
-   public InputStreamDownloadResource(String downloadType, InputStream is, String resourceMimeType)
+   @Override
+   public InputStream getInputStream() throws IOException
    {
-      super(downloadType, resourceMimeType);
-      is_ = is;
+      return null;
    }
 
-   public InputStream getInputStream()
+   @Override
+   public int hashCode()
    {
-      return is_;
+      return id.hashCode();
    }
 }
